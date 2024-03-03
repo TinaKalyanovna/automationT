cc00581efab844cd307f6e671afe28f4bbfbf738
Tina <fatina.kalyanovna@gmail.com>
2024-03-03 15:53:33 +0300

    added loginData

---
+++ HW_3/src/main/java/accuweather/data/LoginData.java
@@ -0,0 +1,7 @@
+public class LoginData {
+    public static final String LOGIN = "testerpurvich@yandex.ru";
+    public static final String PASSWORD = "GeekBrains2021!";
+
+    public static final String INCORRECT_LOGIN = "Tester";
+    public static final String INCORRECT_PASSWORD = "password";
+}
