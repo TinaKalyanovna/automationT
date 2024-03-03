9b42da978ca66e1f06d25e46818255e811936cb9
Tina <fatina.kalyanovna@gmail.com>
2024-03-03 20:45:12 +0300

    added main

---
+++ HW_6/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        UserInterfaceView userInterfaceView = new UserInterfaceView();
+
+        userInterfaceView.runInterface();
+    }
+}
