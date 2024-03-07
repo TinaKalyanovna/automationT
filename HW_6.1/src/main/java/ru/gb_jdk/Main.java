c716d2ebca5266ea2932d089af5db68fab1237f2
Tina <fatina.kalyanovna@gmail.com>
2024-03-07 15:33:18 +0300

    added main

---
+++ HW_6.1/src/main/java/ru/gb_jdk/Main.java
@@ -0,0 +1,9 @@
+import main.java.ru.gb_jdk.Game.Presenter;
+
+public class Main {
+        public static void main(String[] args) {
+        Presenter presenter = new Presenter(1000);
+        presenter.start();
+    }
+
+}
